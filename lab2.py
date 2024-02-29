a78578d008af374fa6806d9ccb25a54261c15316
Jaspreet-Singh14 <160678530+Jaspreet-Singh14@users.noreply.github.com>
2024-02-29 15:00:14 -0500

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,1 +1,11 @@
+import sys
 
+def command_line_arguments():
+    script_name = sys.argv[0]
+    variables_used = sys.argv[1:]
+    
+    print("Script:", script_name)
+    print("Variables used:", variables_used)
+    print("Script and Variables:", script_name, variables_used)
+
+command_line_arguments()
